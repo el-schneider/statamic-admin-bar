3ab80d254de8d3779d22e7e3c087b7dae42619aa
el-schneider <mail@jonaslist.de>
2025-02-11 22:20:33 +0100

    fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -29,6 +29,8 @@ export default typescriptEslint.config(
         files: ['**/resources/js/components/ui/**/*.{ts,vue}'],
         rules: {
             'vue/multi-word-component-names': 'off',
+            '@typescript-eslint/no-unused-vars': 'off',
+            'vue/require-default-prop': 'off',
         },
     },
     eslintConfigPrettier,
--- resources/js/components/ui/switch/Switch.vue
+++ resources/js/components/ui/switch/Switch.vue
@@ -22,7 +22,7 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits)
         v-bind="forwarded"
         :class="
             cn(
-                'peer inline-flex h-4 w-7 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input',
+                'border-transparent peer inline-flex h-4 w-7 shrink-0 cursor-pointer items-center rounded-full border-2 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input',
                 props.class,
             )
         "
