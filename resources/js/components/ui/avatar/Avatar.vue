ee1757774d38c1c9930cd2d451ba098ee085c97a
el-schneider <mail@jonaslist.de>
2025-02-16 15:53:27 +0100

    Fix code style

--- resources/js/components/ui/avatar/Avatar.vue
+++ resources/js/components/ui/avatar/Avatar.vue
@@ -1,22 +1,25 @@
 <script setup lang="ts">
-import type { HTMLAttributes } from 'vue'
-import type { AvatarVariants } from '.'
 import { cn } from '@/lib/utils'
 import { AvatarRoot } from 'radix-vue'
+import type { HTMLAttributes } from 'vue'
+import type { AvatarVariants } from '.'
 import { avatarVariant } from '.'
 
-const props = withDefaults(defineProps<{
-  class?: HTMLAttributes['class']
-  size?: AvatarVariants['size']
-  shape?: AvatarVariants['shape']
-}>(), {
-  size: 'sm',
-  shape: 'circle',
-})
+const props = withDefaults(
+    defineProps<{
+        class?: HTMLAttributes['class']
+        size?: AvatarVariants['size']
+        shape?: AvatarVariants['shape']
+    }>(),
+    {
+        size: 'sm',
+        shape: 'circle',
+    },
+)
 </script>
 
 <template>
-  <AvatarRoot :class="cn(avatarVariant({ size, shape }), props.class)">
-    <slot />
-  </AvatarRoot>
+    <AvatarRoot :class="cn(avatarVariant({ size, shape }), props.class)">
+        <slot />
+    </AvatarRoot>
 </template>
